05af0901373a1e76c7c685725a8f6fe7a7dc001f
Beatriz <Beatriz@MacBook-Pro-de-Beatriz.local>
2017-11-04 15:08:03 +0100

    Iteration 2

--- starter-code/lib/harryPotter.js
+++ starter-code/lib/harryPotter.js
@@ -8,3 +8,27 @@
                    "4 October", "Filius Flitwick", "17 October", "Molly Weasley",
                    "30 October", "Bill Weasley", "29 November", "Rubeus Hagrid",
                    "6 December", "Charlie Weasley", "12 December"];
+
+//METODO 1
+
+var char = birthdays.filter(function(elem, index) {
+  return ixex % 2 === 0;
+});
+
+var char = birthdays.filter(function(elem, index) {
+  return ixex % 2 !== 0;
+});
+
+var newArray = char.map(function(char, index){
+  return [char, dates[index]];
+});
+
+console.log(newArray);
+
+//METODO 2 (más elegante)
+
+var output = birthdays.reduce(function(result, elem, index, array) {
+  if (index % 2 === 0);
+  result.push([elem, array[index +1]]);
+  return result;
+}, []);
